0f8986bb7d3c451c628afabe811e4ac65c2c1ea0
dongchao <3291240985@qq.com>
2020-11-14 11:28:26 +0800

    完成了静态库的制作

---
+++ LinuxSystemCode/静态库的制作/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "mymath.h"
+
+int main()
+{
+  int a=14,b=7;
+
+  printf("%d+%d=%d\n",a,b,add(a,b));
+  printf("%d-%d=%d\n",a,b,sub(a,b));
+  printf("%d/%d=%d\n",a,b,div1(a,b));
+
+  return 0;
+
+}
