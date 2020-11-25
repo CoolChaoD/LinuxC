6a5cc5228ea05a22f266163b8d7fdf23c32aae40
dongchao <3291240985@qq.com>
2020-11-26 00:28:04 +0800

    创建project文件夹，并开始了演讲比赛项目

---
+++ project/speechContest/02_speechManger.cpp
@@ -0,0 +1,13 @@
+#include "02_speechManger.h"
+
+//构造函数
+SpeechManger::SpeechManger()
+{
+
+}
+//析构函数
+SpeechManger::~SpeechManger()
+{
+  
+}
+
---
+++ project/speechContest/02_speechManger.h
@@ -0,0 +1,23 @@
+#ifndef _SPEECHMANGER_H_
+#define _SPEECHMANGER_H_
+#include <iostream>
+using namespace std;
+
+
+//设计演讲管理者类
+class SpeechManger
+{
+  //构造函数
+  SpeechManger();
+  //析构函数
+  ~SpeechManger();
+};
+
+
+
+
+
+
+
+
+#endif
