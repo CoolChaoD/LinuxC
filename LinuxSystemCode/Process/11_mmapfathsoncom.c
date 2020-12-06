00aece566b953e29dda8298e21a0b471130f3f41
dongchao <3291240985@qq.com>
2020-12-06 17:13:07 +0800

    实验室

--- LinuxSystemCode/Process/11_mmapfathsoncom.c
+++ LinuxSystemCode/Process/11_mmapfathsoncom.c
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <sys/wait.h>
 
+#define NUM 1024
 void sys_err(const char *str)
 {
   perror(str);
@@ -20,7 +22,24 @@ int main()
   fd=open("./mmap.txt",O_RDWR|O_TRUNC);
   if(fd<0)
   {
-    sys_err(ZZ)
+    sys_err("open error");
   }
-  p=mmap(NULL,4,)
+  ftruncate("./mmap.txt",NUM); //拓展文件的大小
+  p=mmap(NULL,4,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+  if(p==MAP_FAILED)
+  {
+    sys_err("mmap error");
+  }
+
+  pid=fork(); //fork创建子进程
+  if(pid==0)  //子进程
+  {
+    strcpy(p,"hello pipe"); //对文件进行写操作
+    
+  }else if(pid>0){ //进入了父进程
+    
+    printf("-----%s\n",p);
+    wait(NULL); //回收子进程
+  }
+
 }
