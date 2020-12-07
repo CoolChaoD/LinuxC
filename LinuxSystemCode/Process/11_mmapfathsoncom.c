c1b6b571b5f79550c347f17d4cab24e72db8d459
dongchao <3291240985@qq.com>
2020-12-07 13:17:16 +0800

    mmap

--- LinuxSystemCode/Process/11_mmapfathsoncom.c
+++ LinuxSystemCode/Process/11_mmapfathsoncom.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
+#include <string.h>
 
 #define NUM 1024
 void sys_err(const char *str)
@@ -16,30 +17,34 @@ int main()
 {
   pid_t pid;
   int fd;
-  int *p;
+  char *p;
 
   //首先打开一个文件
-  fd=open("./mmap.txt",O_RDWR|O_TRUNC);
+  fd=open("./mmap.txt",O_RDWR|O_CREAT);
   if(fd<0)
   {
     sys_err("open error");
   }
-  ftruncate("./mmap.txt",NUM); //拓展文件的大小
-  p=mmap(NULL,4,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+  ftruncate(fd,NUM); //拓展文件的大小
+  p=mmap(NULL,NUM,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
   if(p==MAP_FAILED)
   {
     sys_err("mmap error");
   }
-
+  //建立映射区完毕后，即可关闭文件
+  close(fd);
   pid=fork(); //fork创建子进程
   if(pid==0)  //子进程
   {
     strcpy(p,"hello pipe"); //对文件进行写操作
-    
   }else if(pid>0){ //进入了父进程
-    
-    printf("-----%s\n",p);
+    printf("-----%s\n",p); //共享内存
     wait(NULL); //回收子进程
+    int ret=munmap(p,NUM);  //释放映射区
+    if(ret==-1)
+    {
+      sys_err("munmap error");
+    } 
+    return 0;
   }
-
 }
