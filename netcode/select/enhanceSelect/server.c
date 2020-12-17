416e78b040aaff4777668d6764584e583f9a83c0
dongchao <3291240985@qq.com>
2020-12-17 17:04:16 +0800

    正在编写增强select函数，还未完成

--- netcode/select/enhanceSelect/server.c
+++ netcode/select/enhanceSelect/server.c
@@ -4,4 +4,19 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <ctype.h>
 
+#include "wrap.h"
+
+#define SERV_PORT 6666 
+
+int main()
+{
+  int i,j,n,maxi;
+
+  int nready,client[FD_SETSIZE]; //自定义数组client,防止遍历1024个文件描述符， FD_SETSIZE默认为1024
+  int maxfd,listenfd,connfd,sockfd;
+  char bufsize[BUFSIZ],str[INET_ADDRSTRLEN];
+}
